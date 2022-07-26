ab49bc30bbec4da0c78f327bf06697f6cff119f5
Kairos9527 <yuzixiaokairos@gmail.com>
2022-07-26 14:47:03 -0700

    action analysis

---
+++ skeleton/analysis_support.py
@@ -0,0 +1,34 @@
+def important_point_selection(peak, valley):
+
+    peak_cursor = 0
+    valley_cursor = 0
+    node = []
+
+    while peak_cursor < len(peak) or valley_cursor < len(valley):
+
+        if valley_cursor == len(valley):
+            node.append([peak[peak_cursor], 1])
+            peak_cursor += 1
+        elif peak_cursor == len(peak):
+            node.append([valley[valley_cursor], 1])
+            valley_cursor += 1
+        else:
+            if peak[peak_cursor] < valley[valley_cursor]:
+                current_min_index = peak[peak_cursor]
+                node.append([peak[peak_cursor], 1])
+                peak_cursor += 1
+
+            else:
+                current_min_index = valley[valley_cursor]
+                node.append([valley[valley_cursor], 0])
+                valley_cursor += 1
+
+    selected_node = [node[0][0]]
+    pre_flag = node[0][1]
+    for i in node:
+        cur_flag = i[1]
+        if cur_flag != pre_flag:
+            selected_node.append(i[0])
+            pre_flag = cur_flag
+
+    return selected_node
