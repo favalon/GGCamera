2f0e85dd19032576c786a17b2aa8034b3421685e
Favalon <bleedavalon@gmail.com>
2022-10-04 16:01:09 -0700

    pre-process update

--- GAN_train/pre_process.py
+++ GAN_train/pre_process.py
@@ -1,13 +1,39 @@
+import math
 import os
+import random
 
 import numpy as np
 
 from general.save_load import LoadBasic
-from skeleton.main import get_skeleton_array
 
 body_masks_valid = np.array(
     [[1, 1, 1, 1, 0], [1, 1, 1, 1, 1], [1, 1, 1, 1, 0], [1, 1, 1, 1, 0], [1, 1, 1, 1, 1], [1, 1, 1, 1, 1]]).reshape(
     (1, 6, 5))
+
+
+def get_skeleton_array(skeleton_sequence, max_frame=100):
+    all_skeleton_xyz = np.zeros((max_frame, 4, 26, 3))
+    for frame, skeleton in enumerate(skeleton_sequence):
+        if frame >= max_frame:
+            break
+        for i, points in enumerate(skeleton['Skeleton']):
+            all_skeleton_xyz[frame, 0, i, 0] = points['LocalPosition']['x']
+            all_skeleton_xyz[frame, 0, i, 2] = points['LocalPosition']['y']
+            all_skeleton_xyz[frame, 0, i, 1] = points['LocalPosition']['z']
+
+            all_skeleton_xyz[frame, 1, i, 0] = points['LocalRotation']['x']
+            all_skeleton_xyz[frame, 1, i, 1] = points['LocalRotation']['y']
+            all_skeleton_xyz[frame, 1, i, 2] = points['LocalRotation']['z']
+
+            all_skeleton_xyz[frame, 2, i, 0] = points['WorldPosition']['x']
+            all_skeleton_xyz[frame, 2, i, 2] = points['WorldPosition']['y']
+            all_skeleton_xyz[frame, 2, i, 1] = points['WorldPosition']['z']
+
+            all_skeleton_xyz[frame, 3, i, 0] = points['WorldRotation']['x']
+            all_skeleton_xyz[frame, 3, i, 1] = points['WorldRotation']['y']
+            all_skeleton_xyz[frame, 3, i, 2] = points['WorldRotation']['z']
+
+    return all_skeleton_xyz[:, 0:2, :, :]
 
 
 def prep_for_region(body_region, pose):
@@ -95,9 +121,33 @@ def format_camera_data(camera_data):
     return camera_data
 
 
-def pre_process_single_action_data(action_data, camera_data, action_name="default", intensity=1, distance=2, norm=True):
+def random_sign():
+    return random.randint(0, 1) * 2 - 1
+
+
+def random_position_generation(position, distance):
+    x = random_sign() * round(random.random() * 10, 2)
+    y = random_sign() * round(random.random() * 10, 2)
+
+    x_addition = round(random.random() * distance, 2)
+    y_addition = math.sqrt(distance ** 2 - x_addition ** 2)
+
+    # actor_pos
+    position[:, 0, 0] = x
+    position[:, 0, 1] = y
+    # target_pos
+    position[:, 1, 0] = x + random_sign() * x_addition
+    position[:, 1, 1] = y + random_sign() * y_addition
+
+    return position
+
+
+def pre_process_single_action_data(action_data, camera_data,
+                                   action_name="default",
+                                   intensity=1, distance=2, max_frame=100,
+                                   norm=True):
     # 1. raw data
-    skeleton_sequence = get_skeleton_array(action_data)
+    skeleton_sequence = get_skeleton_array(action_data, max_frame=max_frame)
     # 2. raw diff
     diff_skeleton = get_skeleton_diff(skeleton_sequence, norm=norm)
     # 3. raw v
@@ -130,9 +180,14 @@ def pre_process_single_action_data(action_data, camera_data, action_name="defaul
     camera_data = format_camera_data(camera_data["data"])
 
     # 6. intensity (frames, intensity)
+    emo_intensity = np.zeros((max_frame, 1))
+    emo_intensity[:, :] = intensity
     # 7. target distance (frames distance)
+    dis_diff = np.zeros((max_frame, 1))
+    dis_diff[:, :] = distance
     # 8. target and actor position shape(frame, 2, (x, y, z))
-
+    position = np.zeros((max_frame, 2, 3))
+    position = random_position_generation(position, distance)
 
     return skeleton_data, camera_data
 
@@ -152,6 +207,6 @@ if __name__ == '__main__':
     action = LoadBasic.load_basic(fn="actor_data.json", path=data_root, file_type='json')
     camera = LoadBasic.load_basic(fn="camera_output/camera_{}_int_{}.json".format(dis, intensity), path=data_root,
                                   file_type='json')
-    pre_process_single_action_data(action, camera)
+    pre_process_single_action_data(action, camera, distance=dis, intensity=intensity)
 
     print("test end")
